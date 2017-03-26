d9089c2473bc5599f7010ef8fef156f7d38287f7
allurba <allurba@gmail.com>
2017-03-26 14:50:12 +0200

    AospExtended

--- doze/src/com/cyanogenmod/settings/doze/DozeSettings.java
+++ doze/src/com/cyanogenmod/settings/doze/DozeSettings.java
@@ -30,16 +30,5 @@ public class DozeSettings extends SettingsDrawerActivity {
 
         getFragmentManager().beginTransaction().replace(R.id.content_frame,
                 new DozeSettingsFragment()).commit();
-
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
         }
-        return false;
-    }
 }
