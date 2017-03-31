cecd1449abbde0d06342c25707bfaaa1c4ef9c48
allurba <allurba@gmail.com>
2017-03-31 11:14:51 +0200

    aicp

--- device.mk
+++ device.mk
@@ -50,7 +50,6 @@ PRODUCT_COPY_FILES += \
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
@@ -208,9 +207,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/sec_config:system/etc/sec_config
 
 
-# Doze
+# Keyhandler
 PRODUCT_PACKAGES += \
-    RioDoze
+    CMActions \
+    com.cyanogenmod.keyhandler
 
 # Lights
 PRODUCT_PACKAGES += \
